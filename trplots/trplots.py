87b61f68bacdaf902dcfaab6d22a74c55e711631
Y Sakashita <73250095+sakashita44@users.noreply.github.com>
2024-12-10 23:26:38 +0900

    feat: trplots.pyにエクスポート対象を追加

--- trplots/trplots.py
+++ trplots/trplots.py
@@ -15,7 +15,25 @@ from plot_describe import (
     single_describe,
     series_describe,
 )
-from plot_config import configure_ax, set_legend
+from plot_config import configure_ax
+
+__all__ = [
+    "SWARMPLOT_DEFAULTS",
+    "PLOT_DEFAULTS",
+    "TrendPlots",
+    "box_mean_plot",
+    "get_boxwidth",
+    "get_boxcenter_x",
+    "add_brackets_for_boxplot",
+    "plot_bracket",
+    "get_graph_area",
+    "check_bracket",
+    "line_mean_sd_plot",
+    "line_group_coloring_plot",
+    "single_describe",
+    "series_describe",
+    "configure_ax",
+]
 
 if __name__ == "__main__":
     pass
